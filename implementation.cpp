9cafa1aad2e34c154fa2ca61d4385ebfeb73e938
Michał Orynicz <morynicz@gmail.com>
2020-07-11 20:03:41 +0200

    Pass test 3x3 board single central cell get's dead

--- implementation.cpp
+++ implementation.cpp
@@ -1,6 +1,9 @@
 #include "implementation.hpp"
+#include <algorithm>
 
 Board calculateNextGeneration(const Board& in)
 {
-    return in;
+    Board output;
+    std::transform(in.begin(), in.end(), std::back_inserter(output), [](const auto&){return false;});
+    return output;
 }
--- test.cpp
+++ test.cpp
@@ -1,9 +1,16 @@
 #include "gtest/gtest.h"
 #include "implementation.hpp"
 
-TEST(1x1BoardTests, cellStaysDead)
+TEST(Board1x1Tests, cellStaysDead)
 {
     Board input{false};
     Board output{false};
+    EXPECT_EQ(output, calculateNextGeneration(input));
+}
+
+TEST(Board3x3Tests, aliveCellBecomesDead)
+{
+    Board input{false, false, false, false, true, false, false, false, false};
+    Board output{false,false,false, false, false, false, false, false, false};
     EXPECT_EQ(output, calculateNextGeneration(input));
 }
